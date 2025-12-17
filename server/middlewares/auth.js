3c54e1ccb30f75acda2744ab148efbd03644da2f
SaranshGupta-sg <saransh.g2911@gmail.com>
2025-12-17 21:21:25 +0530

    neww

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,15 +1,27 @@
 import jwt from 'jsonwebtoken'
 
-const userAuth = async (req, resizeBy, next) => {
-    const {token} = req.headers;
+const userAuth = async (req, res, next) => {
+    const { token } = req.headers
 
     if (!token) {
-        return res.json ({ success: false, message: 'Not Authorized. Login Again'});
-    } 
+        return res.json({ success: false, message: 'Not Authorized. Login Again' })
+    }
 
     try {
-        const tokenDecode = jwt.verify(token,process.env.JWT_SECRET);
+        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET)
+
+        if (tokenDecode.id) {
+            req.body.userId = tokenDecode.id
+
+        } else {
+            return res.json({ success: false, message: 'Not Authorized. Login Again' })
+        }
+
+        next();
+
     } catch (error) {
-        
+        return res.json({ success: false, message: 'Not Authorized. Login Again' })
     }
 }
+
+export default userAuth
