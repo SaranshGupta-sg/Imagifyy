95b0fbb0fc1f8408f633b2ed4c6bb864add3ec87
SaranshGupta-sg <saransh.g2911@gmail.com>
2025-12-19 01:10:38 +0530

    new

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import userRouter from './routes/userRoutes.js'
+import imageRouter from './routes/imageRoutes.js'
 
 const PORT = process.env.PORT || 4000
 const app = express()
@@ -12,9 +13,12 @@ app.use(cors())
 
 await connectDB()
 
+app.get('/', (req, res) => res.send("API Working"))
+
 // ✅ USER ROUTES
 app.use('/api/user', userRouter)
+app.use('/api/image', imageRouter)
 
-app.get('/', (req, res) => res.send("API Working"))
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
